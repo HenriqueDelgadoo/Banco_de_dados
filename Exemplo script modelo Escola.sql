bf0a9885e5aaff7c9a2a9785f9200740c552178f
henriquedelgadoo <henriquedelgado055@gmail.com>
2025-08-08 16:35:30 -0300

    AULA 02

--- Exemplo script modelo Escola.sql
+++ Exemplo script modelo Escola.sql
@@ -1,13 +1,13 @@
-create database modelo; -- cadastro de base de dados
+create database pizzaria; -- cadastro de base de dados
 
-use modelo; -- Especifica a base de dados que irá ultilizar
+use pizzaria; -- Especifica a base de dados que irá ultilizar
 
-create table aluno (
-id_aluno int primary key
-); -- cria a tabela chamada aluno, com a id_aluno como chave primária 
+create table usuario (
+id_usuario int primary key
+); -- cria a tabela chamada aluno, com a id_usuario como chave primária 
 create table funcionario (
 id_funcionario int primary key
-); -- cria a tabela funcionario com o id_funcionario como chave primária 
-create table Curso (
-id_Curso int primary key
-); -- cria a tabela curso com a chave primária de id_curso
+); -- cria a tabela funcionario com o id_funcionario  como chave primária 
+create table pizza (
+id_pizza int primary key
+); -- cria a tabela curso com a chave primária de id_pizza
